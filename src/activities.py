03db62f99650e5d717b6ee32a11819e9a7990d56
Rippenkneifer <147978010+Rippenkneifer@users.noreply.github.com>
2024-08-27 11:57:03 +0200

    Update activities.py

--- src/activities.py
+++ src/activities.py
@@ -1,11 +1,10 @@
-import contextlib
+import logging
 import random
 import time
-
-from selenium.common import TimeoutException
+from bdb import effective
+from time import sleep
+from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-
 from src.browser import Browser
 
 
@@ -13,6 +12,28 @@ class Activities:
     def __init__(self, browser: Browser):
         self.browser = browser
         self.webdriver = browser.webdriver
+    def click_element_if_visible(self, element):
+        try:
+            if element.is_displayed() and element.is_enabled():
+                element.click()
+                logging.info("Dashboard pop-up registered and closed needs to be done once on new accounts")
+            else:
+                pass
+        except (ElementNotInteractableException, NoSuchElementException):
+            pass
+    def dashboardPopUpModalCloseCross(self):
+        try:
+            element = self.webdriver.find_element(By.XPATH,
+                                                  "//div[@id=\'rewardsBanner\']/div/div/div[2]/div[4]/"
+                                                  "mee-rewards-user-status-banner-item/mee-rewards-user-status-banner-streak/mee-rewards-pop-up/div/div[2]/div[3]/a")
+            self.click_element_if_visible(element)
+            time.sleep(0.25)
+        except NoSuchElementException:
+            pass
+
+
+
+
 
     def openDailySetActivity(self, cardId: int):
         # Open the Daily Set activity for the given cardId
@@ -30,112 +51,88 @@ class Activities:
 
     def completeSearch(self):
         # Simulate completing a search activity
-        time.sleep(random.randint(10, 15))
+        time.sleep(3)
         self.browser.utils.closeCurrentTab()
 
     def completeSurvey(self):
         # Simulate completing a survey activity
         # noinspection SpellCheckingInspection
         self.webdriver.find_element(By.ID, f"btoption{random.randint(0, 1)}").click()
-        time.sleep(random.randint(10, 15))
+        time.sleep(1.5)
         self.browser.utils.closeCurrentTab()
 
     def completeQuiz(self):
         # Simulate completing a quiz activity
-        with contextlib.suppress(TimeoutException):
-            startQuiz = self.browser.utils.waitUntilQuizLoads()
-            self.browser.utils.click(startQuiz)
-        self.browser.utils.waitUntilVisible(
-            By.ID, "overlayPanel", 5
-        )
-        currentQuestionNumber: int = self.webdriver.execute_script(
-            "return _w.rewardsQuizRenderInfo.currentQuestionNumber"
-        )
-        maxQuestions = self.webdriver.execute_script(
-            "return _w.rewardsQuizRenderInfo.maxQuestions"
-        )
-        numberOfOptions = self.webdriver.execute_script(
-            "return _w.rewardsQuizRenderInfo.numberOfOptions"
-        )
-        for _ in range(currentQuestionNumber, maxQuestions + 1):
+        startQuiz = self.browser.utils.waitUntilQuizLoads()
+        startQuiz.click()
+        self.browser.utils.waitUntilVisible(By.XPATH, '//*[@id="currentQuestionContainer"]/div/div[1]', 4)
+        time.sleep(2)
+        numberOfQuestions = self.webdriver.execute_script("return _w.rewardsQuizRenderInfo.maxQuestions")
+        numberOfOptions = self.webdriver.execute_script("return _w.rewardsQuizRenderInfo.numberOfOptions")
+        for question in range(numberOfQuestions):
             if numberOfOptions == 8:
                 answers = []
                 for i in range(numberOfOptions):
-                    isCorrectOption = self.webdriver.find_element(
-                        By.ID, f"rqAnswerOption{i}"
-                    ).get_attribute("iscorrectoption")
+                    isCorrectOption = self.webdriver.find_element(By.ID, f"rqAnswerOption{i}").get_attribute("iscorrectoption")
                     if isCorrectOption and isCorrectOption.lower() == "true":
                         answers.append(f"rqAnswerOption{i}")
                 for answer in answers:
-                    element = self.webdriver.find_element(By.ID, answer)
-                    self.browser.utils.click(element)
+                    self.webdriver.find_element(By.ID, answer).click()
+                    time.sleep(1)
                     self.browser.utils.waitUntilQuestionRefresh()
             elif numberOfOptions in [2, 3, 4]:
                 correctOption = self.webdriver.execute_script(
-                    "return _w.rewardsQuizRenderInfo.correctAnswer"
-                )
+                    "return _w.rewardsQuizRenderInfo.correctAnswer")
                 for i in range(numberOfOptions):
-                    if (
-                        self.webdriver.find_element(
-                            By.ID, f"rqAnswerOption{i}"
-                        ).get_attribute("data-option")
-                        == correctOption
-                    ):
-                        element = self.webdriver.find_element(By.ID, f"rqAnswerOption{i}")
-                        self.browser.utils.click(element)
-
+                    if (self.webdriver.find_element(By.ID, f"rqAnswerOption{i}").get_attribute("data-option") == correctOption):
+                        self.webdriver.find_element(By.ID, f"rqAnswerOption{i}").click()
+                        time.sleep(1.2)
                         self.browser.utils.waitUntilQuestionRefresh()
                         break
+            if question + 1 != numberOfQuestions:
+                time.sleep(2.5)
+        time.sleep(3.5)
         self.browser.utils.closeCurrentTab()
 
     def completeABC(self):
         # Simulate completing an ABC activity
-        counter = self.webdriver.find_element(
-            By.XPATH, '//*[@id="QuestionPane0"]/div[2]'
-        ).text[:-1][1:]
+        counter = self.webdriver.find_element(By.XPATH, '//*[@id="QuestionPane0"]/div[2]').text[:-1][1:]
         numberOfQuestions = max(int(s) for s in counter.split() if s.isdigit())
         for question in range(numberOfQuestions):
-            element = self.webdriver.find_element(By.ID, f"questionOptionChoice{question}{random.randint(0, 2)}")
-            self.browser.utils.click(element)
-            time.sleep(random.randint(10, 15))
-            element = self.webdriver.find_element(By.ID, f"nextQuestionbtn{question}")
-            self.browser.utils.click(element)
-            time.sleep(random.randint(10, 15))
-        time.sleep(random.randint(1, 7))
+            self.webdriver.find_element(By.ID, f"questionOptionChoice{question}{random.randint(0, 2)}").click()
+            time.sleep(1.5)
+            self.webdriver.find_element(By.ID, f"nextQuestionbtn{question}").click()
+            time.sleep(1.5)
+        time.sleep(1.5)
         self.browser.utils.closeCurrentTab()
 
     def completeThisOrThat(self):
         # Simulate completing a This or That activity
         startQuiz = self.browser.utils.waitUntilQuizLoads()
-        self.browser.utils.click(startQuiz)
-        self.browser.utils.waitUntilVisible(
-            By.XPATH, '//*[@id="currentQuestionContainer"]/div/div[1]', 10
-        )
-        time.sleep(random.randint(10, 15))
+        startQuiz.click()
+        self.browser.utils.waitUntilVisible(By.XPATH, '//*[@id="currentQuestionContainer"]/div/div[1]', 10)
+        time.sleep(4)
         for _ in range(10):
-            correctAnswerCode = self.webdriver.execute_script(
-                "return _w.rewardsQuizRenderInfo.correctAnswer"
-            )
+            correctAnswerCode = self.webdriver.execute_script("return _w.rewardsQuizRenderInfo.correctAnswer")
             answer1, answer1Code = self.getAnswerAndCode("rqAnswerOption0")
             answer2, answer2Code = self.getAnswerAndCode("rqAnswerOption1")
-            answerToClick: WebElement
             if answer1Code == correctAnswerCode:
-                answerToClick = answer1
+                answer1.click()
+                time.sleep(2)
             elif answer2Code == correctAnswerCode:
-                answerToClick = answer2
+                answer2.click()
+                time.sleep(2)
 
-            self.browser.utils.click(answerToClick)
-            time.sleep(random.randint(10, 15))
-
-        time.sleep(random.randint(10, 15))
+        time.sleep(4)
         self.browser.utils.closeCurrentTab()
 
-    def getAnswerAndCode(self, answerId: str) -> tuple[WebElement, str]:
+    def getAnswerAndCode(self, answerId: str) -> tuple:
         # Helper function to get answer element and its code
         answerEncodeKey = self.webdriver.execute_script("return _G.IG")
         answer = self.webdriver.find_element(By.ID, answerId)
         answerTitle = answer.get_attribute("data-option")
-        return (
-            answer,
-            self.browser.utils.getAnswerCode(answerEncodeKey, answerTitle),
-        )
+        if answerTitle is not None:
+            return (answer,self.browser.utils.getAnswerCode(answerEncodeKey, answerTitle),)
+        else:
+            # todo - throw exception?
+            return answer, None
